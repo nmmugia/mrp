9cb992ca879acb9f2cc8f84151d8ff3cc06c7f3b
Mugia NM <seemugia1@gmail.com>
2019-09-18 17:06:48 +0700

    Added datatables to list finished product and consumed product on MO

--- src/screens/AddMo/component.js
+++ src/screens/AddMo/component.js
@@ -1,19 +1,66 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 import PropTypes from 'prop-types';
+import { FAB, DataTable } from 'react-native-paper';
 import { Container, Header, Tabs, Tab, ScrollableTab } from 'native-base';
 // import CaribeScanner from './caribe';
 import Form from './form';
 
 class Component extends React.Component {
+  state = {
+    active: false,
+  };
+
+  componentWillMount() {
+    const { actions, token, id } = this.props;
+    actions.getMOById({ token, id });
+  }
+
   handleSubmit = values => {
     const { actions } = this.props;
-    console.log(values);
     actions.addMo(values);
   };
 
   render() {
+    const { data } = this.props;
+    const { active } = this.state;
+    let raw_ids, finished_ids = null;
+    if (data) {
+      // eslint-disable-next-line camelcase
+      const { move_raw_ids, move_finished_ids } = data;
+      raw_ids = Object.keys(move_raw_ids).map(i => {
+        return (
+          <DataTable.Row key={i}>
+            <DataTable.Cell style={{ width: 100, paddingRight: 20 }}>
+              {move_raw_ids[i].name}
+            </DataTable.Cell>
+            <DataTable.Cell style={{ width: 150, paddingRight: 20 }}>
+              {move_raw_ids[i].product_uom_qty}
+            </DataTable.Cell>
+            <DataTable.Cell style={{ width: 100, paddingRight: 20 }}>
+              {move_raw_ids[i].reserved_availability}
+            </DataTable.Cell>
+            <DataTable.Cell style={{ width: 150, paddingRight: 20 }}>
+              {move_raw_ids[i].qty_done}
+            </DataTable.Cell>
+          </DataTable.Row>
+        );
+      });
+
+      finished_ids = Object.keys(move_finished_ids).map(i => {
+        return (
+          <DataTable.Row key={i}>
+            <DataTable.Cell style={{ width: 100, paddingRight: 20 }}>
+              {move_finished_ids[i].name}
+            </DataTable.Cell>
+            <DataTable.Cell style={{ width: 150, paddingRight: 20 }}>
+              {move_finished_ids[i].qty_done}
+            </DataTable.Cell>
+          </DataTable.Row>
+        );
+      });
+    }
     return (
       <Container>
         <Header style={{ height: 1, backgroundColor: '#7c7bad' }} hasTabs />
@@ -26,7 +73,7 @@ class Component extends React.Component {
               heading="Main Data"
             >
               <ScrollView>
-                <Form onSubmit={this.handleSubmit} />
+                <Form initialValues={data} onSubmit={this.handleSubmit} />
               </ScrollView>
             </Tab>
             <Tab
@@ -36,7 +83,17 @@ class Component extends React.Component {
               heading="Consumed Materials"
             >
               <ScrollView>
-                <Form onSubmit={this.handleSubmit} />
+                <DataTable.Header>
+                  <DataTable.Title
+                    onPress={() => setSort([1, sort[0] == 1 && sort[1] == true? false: true])}
+                  >
+                    Product
+                  </DataTable.Title>
+                  <DataTable.Title>To Consume</DataTable.Title>
+                  <DataTable.Title>Reserved</DataTable.Title>
+                  <DataTable.Title>Consumed</DataTable.Title>
+                </DataTable.Header>
+                {raw_ids}
               </ScrollView>
             </Tab>
             <Tab
@@ -46,20 +103,43 @@ class Component extends React.Component {
               heading="Finished Materials"
             >
               <ScrollView>
-                <Form onSubmit={this.handleSubmit} />
-              </ScrollView>
-            </Tab>
-            <Tab
-              activeTabStyle={{ backgroundColor: '#5f5e97' }}
-              tabStyle={{ backgroundColor: '#7c7bad' }}
-              textStyle={{ color: '#fff' }}
-              heading="Misc."
-            >
-              <ScrollView>
-                <Form onSubmit={this.handleSubmit} />
+                {!data ? (
+                  <Text>
+                    Use the Produce button or process the work orders to create some finished
+                    products.
+                  </Text>
+                ) : (
+                  <>
+                    <DataTable.Header>
+                      <DataTable.Title
+                        onPress={() => setSort([1, sort[0] == 1 && sort[1] == true? false: true])}
+                      >
+                        Product
+                      </DataTable.Title>
+                      <DataTable.Title
+                        onPress={() => setSort([2, sort[0] == 2 && sort[1] == true? false: true])}
+                      >
+                        Done
+                      </DataTable.Title>
+                    </DataTable.Header>
+                    {finished_ids}
+                  </>
+                )}
               </ScrollView>
             </Tab>
           </Tabs>
+          <FAB.Group
+            fabStyle={{ backgroundColor: '#7c7bad' }}
+            open={active}
+            icon="menu"
+            actions={[
+              { icon: 'help', label: 'Availability', onPress: () => console.log('Pressed star') },
+              { icon: 'add', label: 'WO', onPress: () => console.log('Pressed notifications') },
+              { icon: 'close', label: 'Cancel', onPress: () => console.log('Pressed email') },
+            ]}
+            onStateChange={() => {}}
+            onPress={() => this.setState({ active: !active })}
+          />
         </View>
       </Container>
     );
@@ -68,6 +148,9 @@ class Component extends React.Component {
 
 Component.propTypes = {
   actions: PropTypes.object,
+  token: PropTypes.string,
+  id: PropTypes.string,
+  data: PropTypes.object,
 };
 
 export default Component;
